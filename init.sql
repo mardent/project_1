230e5c964bfd5a6f2dfb1f200ba682497f5b16c9
mardent <mar.dent@yandex.ru>
2015-10-16 15:07:08 +0300

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,1 +1,52 @@
-1
+-- phpMyAdmin SQL Dump
+-- version 4.0.10.10
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Окт 15 2015 г., 17:13
+-- Версия сервера: 5.5.45
+-- Версия PHP: 5.4.44
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `WEBDEV_DB`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `USERS`
+--
+
+CREATE TABLE IF NOT EXISTS `USERS` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `LOGIN` varchar(30) NOT NULL,
+  `PASSWORD` varchar(256) NOT NULL,
+  `NAME` varchar(50) NOT NULL,
+  `SECOND_NAME` varchar(50) NOT NULL,
+  `EMAIL` varchar(50) NOT NULL,
+  `PHONE` varchar(20) DEFAULT NULL,
+  `SOLT` varchar(128) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `LOGIN` (`LOGIN`),
+  UNIQUE KEY `EMAIL` (`EMAIL`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=40 ;
+
+--
+-- Дамп данных таблицы `USERS`
+--
+
+INSERT INTO `USERS` (`ID`, `LOGIN`, `PASSWORD`, `NAME`, `SECOND_NAME`, `EMAIL`, `PHONE`, `SOLT`) VALUES
+(39, 'Marek', '43708a77b8fff0aab6c1d0e22edaf206a0b48c63', 'Marek', 'Pol', 'mar.dent@yandex.ru', '20100901', '270a4fa960aa393736a6182b18fc3dd7');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
